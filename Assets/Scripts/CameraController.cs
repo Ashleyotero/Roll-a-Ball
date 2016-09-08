2d767a023184f870b4e6f77e962bbb3a5b35f514
Ashley Otero <chun.jin.ling@gmail.com>
2016-09-08 12:39:17 -0600

    I literally give up

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,43 +3,22 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject player1;
+	public GameObject player;
 	public GameObject player2;
 
-	private const float DISTANCE_MARGIN = 1.0f;
-
-	private Vector3 middlePoint;
-	private float distanceFromMiddlePoint;
-	private float distanceBetweenPlayers;
-	private float cameraDistance;
-	private float aspectRatio;
-	private float fov;
-	private float tanFov;
+	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-		aspectRatio = Screen.width / Screen.height;
-		tanFov = Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView / 2.0f);
+		offset = transform.position - player.transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void LateUpdate () {
-
-		// Position the camera in the center.
-		Vector3 newCameraPos = Camera.main.transform.position;
-		newCameraPos.x = middlePoint.x;
-		Camera.main.transform.position = newCameraPos;
-
-		// Find the middle point between players.
-		Vector3 vectorBetweenPlayers = player2.transform.position - player1.transform.position;
-		middlePoint = player1.transform.position + 0.5f * vectorBetweenPlayers;
-
-		// Calculate the new distance.
-		distanceBetweenPlayers = vectorBetweenPlayers.magnitude;
-		cameraDistance = (distanceBetweenPlayers / 2.0f / aspectRatio) / tanFov;
-
-		// Set camera to new position.
-		Vector3 dir = (Camera.main.transform.position - middlePoint).normalized;
-		Camera.main.transform.position = middlePoint + dir * (cameraDistance + DISTANCE_MARGIN);
+		transform.position = player.transform.position + offset;
 	}
 }
+
+// note: I worked on this camera part for a good two days and 
+// ended up reverting back to this since it actually worked.
+// I GIVE UP. :)
---
+++ Assets/Scripts/FindMidPoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindMidPoint : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+	private Vector3 midpoint;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		midpoint = (player1.gameObject.transform.position + player2.gameObject.transform.position) * .5f;
+		transform.position = midpoint;
+
+
+	}
+}
